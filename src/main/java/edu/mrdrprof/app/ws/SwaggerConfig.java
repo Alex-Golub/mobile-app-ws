eebb6a89a6ef2d27d10533fb19d5fbb88ff6ec67
Alex Golub <55350522+Alex-Golub@users.noreply.github.com>
2021-04-06 13:15:02 +0300

    customize Swagger-ui welcome page to display relevant info about this web-service

--- src/main/java/edu/mrdrprof/app/ws/SwaggerConfig.java
+++ src/main/java/edu/mrdrprof/app/ws/SwaggerConfig.java
@@ -4,9 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * Configure Swagger where to look for
@@ -18,10 +23,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+  Contact contact = new Contact(
+          "Alex Go",
+          "https://www.youtube.com/watch?v=SkgTxQm9DWM",
+          "email@email.com"
+  );
+
+  ApiInfo apiInfo = new ApiInfo(
+          "Mobile-App RESTful Web-Service Documentation",
+          "This page document Mobile-App RESTful Web-Service end points",
+          "1.0",
+          "#",
+          contact,
+          "Apache 2.0",
+          "http://www.apache.org/licenses/LICENSE-2.0",
+          Collections.emptyList()
+  );
 
   @Bean
   public Docket apiDocket() {
     return new Docket(DocumentationType.SWAGGER_2)
+            .protocols(Set.of("HTTP", "HTTPS")) // supported request protocols
+            .apiInfo(apiInfo) // customize swagger info page
             .select()
             .apis(RequestHandlerSelectors.basePackage("edu.mrdrprof.app.ws"))
             .paths(PathSelectors.any()) // consider all methods of this application
